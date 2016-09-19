8ad71b3084081fad945020d54e35b6533c6030e4
Greg Caporaso <gregcaporaso@gmail.com>
2016-09-19 14:33:44 -0700

    REVERT: feature ids are sequences again

--- q2_dada2/_denoise.py
+++ q2_dada2/_denoise.py
@@ -6,7 +6,6 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
-import hashlib
 import os.path
 import tempfile
 import subprocess
@@ -19,7 +18,7 @@ from q2_types.per_sample_sequences import (
 
 
 def denoise(demultiplexed_seqs: SingleLanePerSampleSingleEndFastqDirFmt,
-            trunc_len: int, trim_left:int) -> (biom.Table, DNAIterator):
+            trunc_len: int, trim_left: int) -> (biom.Table, DNAIterator):
     with tempfile.TemporaryDirectory() as temp_dir_name:
         biom_fp = os.path.join(temp_dir_name, 'output.tsv.biom')
         cmd = ['run_dada.R', str(demultiplexed_seqs), biom_fp,
@@ -31,12 +30,7 @@ def denoise(demultiplexed_seqs: SingleLanePerSampleSingleEndFastqDirFmt,
         # them the sample id part of the filename here.
         sid_map = {id_: id_.split('_')[0] for id_ in table.ids(axis='sample')}
         table.update_ids(sid_map, axis='sample', inplace=True)
-        # Currently the feature IDs in DADA2 are the sequences themselves.
-        # We make them md5 sums of the sequences here.
-        fid_map = {id_: hashlib.md5(id_.encode('utf-8')).hexdigest()
-                   for id_ in table.ids(axis='observation')}
-        table.update_ids(fid_map, axis='observation', inplace=True)
-
-        rep_sequences = DNAIterator((skbio.DNA(k, metadata={'id': v})
-                                     for k, v in fid_map.items()))
+        # The feature IDs are the sequences themselves.
+        rep_sequences = DNAIterator((skbio.DNA(id_, metadata={'id': id_})
+                                     for id_ in table.ids(axis='observation')))
     return table, rep_sequences
