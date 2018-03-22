2f36d3fc3955f2f130e7e6d862469b5fde677d36
Matthew Dillon <matthewrdillon@gmail.com>
2018-03-22 14:01:01 +0200

    ENH: adds support for denoising pyrosequencing reads (#91)
    Fixes #76

--- q2_dada2/plugin_setup.py
+++ q2_dada2/plugin_setup.py
@@ -212,3 +212,86 @@ plugin.methods.register_function(
     description=('This method denoises paired-end sequences, dereplicates '
                  'them, and filters chimeras.')
 )
+
+
+plugin.methods.register_function(
+    function=q2_dada2.denoise_pyro,
+    inputs={'demultiplexed_seqs': SampleData[SequencesWithQuality]},
+    parameters={'trunc_len': qiime2.plugin.Int,
+                'trim_left': qiime2.plugin.Int,
+                'max_ee': qiime2.plugin.Float,
+                'trunc_q': qiime2.plugin.Int,
+                'max_len': qiime2.plugin.Int,
+                'chimera_method': qiime2.plugin.Str %
+                qiime2.plugin.Choices(_CHIM_OPT),
+                'min_fold_parent_over_abundance': qiime2.plugin.Float,
+                'n_threads': qiime2.plugin.Int,
+                'n_reads_learn': qiime2.plugin.Int,
+                'hashed_feature_ids': qiime2.plugin.Bool},
+    outputs=[('table', FeatureTable[Frequency]),
+             ('representative_sequences', FeatureData[Sequence])],
+    input_descriptions={
+        'demultiplexed_seqs': 'The single-end demultiplexed pyrosequencing '
+                              'sequences (e.g. 454, IonTorrent) to be '
+                              'denoised.'
+    },
+    parameter_descriptions={
+        'trunc_len': 'Position at which sequences should be truncated due to '
+                     'decrease in quality. This truncates the 3\' end of the '
+                     'of the input sequences, which will be the bases that '
+                     'were sequenced in the last cycles. Reads that are '
+                     'shorter than this value will be discarded. If 0 is '
+                     'provided, no truncation or length filtering will be '
+                     'performed',
+        'trim_left': 'Position at which sequences should be trimmed due to '
+                     'low quality. This trims the 5\' end of the '
+                     'of the input sequences, which will be the bases that '
+                     'were sequenced in the first cycles.',
+        'max_ee': 'Reads with number of expected errors higher than this '
+                  'value will be discarded.',
+        'trunc_q': 'Reads are truncated at the first instance of a quality '
+                   'score less than or equal to this value. If the resulting '
+                   'read is then shorter than `trunc_len`, it is discarded.',
+        'max_len': 'Remove reads prior to trimming or truncation which are '
+                   'longer than this value. If 0 is provided no reads will '
+                   'be removed based on length.',
+        'chimera_method': 'The method used to remove chimeras. '
+                          '"none": No chimera removal is performed. '
+                          '"pooled": All reads are pooled prior to chimera '
+                          'detection. "consensus": Chimeras are detected in '
+                          'samples individually, and sequences found '
+                          'chimeric in a sufficient fraction of samples are '
+                          'removed.',
+        'min_fold_parent_over_abundance':
+            'The minimum abundance of potential parents of a sequence being '
+            'tested as chimeric, expressed as a fold-change versus the '
+            'abundance of the sequence being tested. Values should be greater '
+            'than or equal to 1 (i.e. parents should be more abundant than '
+            'the sequence being tested). This parameter has no effect if '
+            'chimera_method is "none".',
+        'n_threads': 'The number of threads to use for multithreaded '
+                     'processing. If 0 is provided, all available cores will '
+                     'be used.',
+        'n_reads_learn': 'The number of reads to use when training the '
+                         'error model. Smaller numbers will result in a '
+                         'shorter run time but a less reliable error '
+                         'model.',
+        'hashed_feature_ids': 'If true, the feature ids in the resulting '
+                              'table will be presented as hashes of the '
+                              'sequences defining each feature. The hash '
+                              'will always be the same for the same sequence '
+                              'so this allows feature tables to be merged '
+                              'across runs of this method. You should only '
+                              'merge tables if the exact same parameters are '
+                              'used for each run.'
+    },
+    output_descriptions={
+        'table': 'The resulting feature table.',
+        'representative_sequences': 'The resulting feature sequences. Each '
+                                    'feature in the feature table will be '
+                                    'represented by exactly one sequence.'
+    },
+    name='Denoise and dereplicate single-end pyrosequences',
+    description='This method denoises single-end pyrosequencing sequences, '
+                'dereplicates them, and filters chimeras.'
+)
