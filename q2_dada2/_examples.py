6efa73dc954be088eeee849a5d9aeae5c0322fc5
Liz Gehret <elizabeth.gehret@gmail.com>
2022-11-22 14:10:02 -0700

    debug: reverting data URL back to docs

--- q2_dada2/_examples.py
+++ q2_dada2/_examples.py
@@ -10,11 +10,12 @@ import qiime2
 
 
 demux_single_url = \
-    (f'https://data.qiime2.org/{qiime2.__release__}/tutorials/'
+    (f'https://docs.qiime2.org/{qiime2.__release__}/data/tutorials/'
      'moving-pictures/demux.qza')
+print(demux_single_url)
 
 demux_paired_url = \
-    (f'https://data.qiime2.org/{qiime2.__release__}/tutorials/'
+    (f'https://docs.qiime2.org/{qiime2.__release__}/data/tutorials/'
      'atacama-soils/demux-full.qza')
 
 
