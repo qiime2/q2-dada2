edfab362d99f852cf7ccd4d2e893088135c19d75
Oddant1 <oddant1@hotmail.com>
2026-08-18 11:43:51 -0700

    remove unnecessary stuff in __init__

--- q2_dada2/_dada_stats/__init__.py
+++ q2_dada2/_dada_stats/__init__.py
@@ -6,12 +6,6 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
+from ._visualizer import plot_base_transitions
+
 __all__ = ['plot_base_transitions']
-
-
-def __getattr__(name):
-    if name == 'plot_base_transitions':
-        from ._visualizer import plot_base_transitions
-        return plot_base_transitions
-
-    raise AttributeError(f'module {__name__!r} has no attribute {name!r}')
